049094cde4ac592dfeb52041624ed4f57e81683a
Vinyl <86006069+VinylKK@users.noreply.github.com>
2021-10-20 20:05:17 +0200

    Update pacflag.lua

--- pacflag.lua
+++ pacflag.lua
@@ -1,5 +1,5 @@
 PLUGIN.name = "PAC3 Flag"
-PLUGIN.author = "NS: Alex_07 IX:Vinyl"
+PLUGIN.author = "NS: Alex_07 IX: Vinyl"
 PLUGIN.description = "Simple PAC3 flag and PACban command for player."
 
 ix.flag.Add("Z", "Gives access to the PAC3 editor.")
